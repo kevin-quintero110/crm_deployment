38144400994323266ea0507f3b32b2560974b41b
kevin-quintero110 <167016322+kevin-quintero110@users.noreply.github.com>
2026-08-14 16:24:31 -0500

    fix vercel backend dependencies

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,9 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../backend/index');
 
-const handler = serverless(app);
-module.exports = (req, res) => {
-    res.status(200).json({
-        mensaje: "API Vercel funcionando"
-    });
-};
+module.exports = serverless(app);
