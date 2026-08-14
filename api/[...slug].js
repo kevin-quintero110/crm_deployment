43c88b3ccb9e795dd06d32d78c6c6ffe6cc3968c
kevin-quintero110 <167016322+kevin-quintero110@users.noreply.github.com>
2026-08-14 16:10:16 -0500

    ultima prueba

--- api/[...slug].js
+++ api/[...slug].js
@@ -2,11 +2,8 @@ const serverless = require('serverless-http');
 const app = require('../backend/index');
 
 const handler = serverless(app);
-
 module.exports = (req, res) => {
-    if (req.url && req.url.startsWith('/api')) {
-        req.url = req.url.replace(/^\/api/, '') || '/';
-    }
-
-    return handler(req, res);
+    res.status(200).json({
+        mensaje: "API Vercel funcionando"
+    });
 };
